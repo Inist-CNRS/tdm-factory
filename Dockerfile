a97f4e6f0dd1b06079369f9f8e06c096fb44a79b
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-02-08 13:35:09 +0100

    fix: update backend to use host from config

--- Dockerfile
+++ Dockerfile
@@ -38,6 +38,20 @@ RUN npm run build
 
 #######################################################
 
+# Stage 1 - Building Stage: Build back-end node modules
+FROM node:18.19-bullseye-slim AS node-modules
+
+# Switch to the main working directory
+WORKDIR /app
+
+# Copy package.json, package-lock.json and tsconfig.json files
+COPY tdm-be/package*.json tdm-be/ts*.json /app/
+
+# Install production dependency
+RUN npm install --omit=dev
+
+#######################################################
+
 # Stage 2 - Application Stage: Build the application image
 FROM node:18.19-bullseye-slim AS application
 
@@ -59,7 +73,7 @@ WORKDIR /app
 # Copy back-end files from the build container
 COPY --chown=daemon:daemon --from=express-build /app/package.json /app/
 COPY --chown=daemon:daemon --from=express-build /app/package-lock.json /app/
-COPY --chown=daemon:daemon --from=express-build /app/node_modules /app/node_modules/
+COPY --chown=daemon:daemon --from=node-modules /app/node_modules /app/node_modules/
 COPY --chown=daemon:daemon --from=express-build /app/swagger-config.json /app/
 COPY --chown=daemon:daemon --from=express-build /app/swagger.json /app/
 COPY --chown=daemon:daemon --from=express-build /app/config.json /app/
--- tdm-be/routers/webhook.ts
+++ tdm-be/routers/webhook.ts
@@ -1,63 +1,105 @@
-import express from 'express';
-import { sendEmail, EmailOptions } from '../service/email-sender';
-import { Traitment } from '../model/Traitment';
-import axios from 'axios';
-import fs from 'fs';
 import { StatusEnum } from '../model/StatusEnum';
-import { SwaggerApi } from '../model/Config';
-const router = express.Router();
-var currentTraitments: Traitment[] = require('../model/Traitment');
-var configModel = require('../model/Config');
+import { sendEmail } from '../service/email-sender';
+import axios from 'axios';
+import express from 'express';
+import fs from 'fs';
+import type { SwaggerApi } from '../model/Config';
+import type { Traitment } from '../model/Traitment';
+import type { EmailOptions } from '../service/email-sender';
 
-const config = configModel.getConfig()
+const router = express.Router();
+const configModel = require('../model/Config');
+let currentTraitments: Traitment[] = require('../model/Traitment');
+
+const config = configModel.getConfig();
 
 //Webhook of success after traitment done
-router.post('/success', (req, res) => {
-    const { id } = req.query;
-    const traitment: Traitment = currentTraitments.filter((t) => t.timestamp + '' === id)[0];
-    const enrichment = config.enrichments.filter((enrichment: SwaggerApi) => traitment.enrichment.url.indexOf(enrichment.url) > -1)[0];
-    if (traitment) {
-        axios.post(enrichment.url + enrichment.retrieveUrl, [{ value: traitment.retrieveValue }], { responseType: 'arraybuffer' }).then((retrieveRes) => {
-            traitment.status = StatusEnum.FINISHED;
-            // Process the payload as needed
-            const bin: Buffer = Buffer.from(retrieveRes.data, 'binary');
-            fs.writeFileSync(`public/downloads/${traitment.timestamp}.tar.gz`, bin);
-            console.log('mail sent to smtp');
-            const mailOptions: EmailOptions = {
-                to: traitment.mail,
-                subject: config.mailSuccess.subject,
-                text: config.mailSuccess.text + `\n ${req.protocol}://${req.hostname}/downloads/${traitment.timestamp}.tar.gz`,
-            };
-            sendEmail(mailOptions);
-            res.send('Email envoyé');
-        }, (error) => {
-            console.log(error);
-            res.status(500).send('Error retrieving file');
-        });
-    } else {
-        res.status(404).send(`This traitment doesn't exist on current process id=${id}`);
-    }
-}, (error) => {
-    console.log(error);
-});
-
+router.post(
+    '/success',
+    (req, res) => {
+        const { id } = req.query;
+        const traitment: Traitment = currentTraitments.filter(
+            (t) => t.timestamp + '' === id,
+        )[0];
+        const enrichment = config.enrichments.filter(
+            (enrichment: SwaggerApi) =>
+                traitment.enrichment.url.indexOf(enrichment.url) > -1,
+        )[0];
+        if (traitment) {
+            axios
+                .post(
+                    enrichment.url + enrichment.retrieveUrl,
+                    [{ value: traitment.retrieveValue }],
+                    { responseType: 'arraybuffer' },
+                )
+                .then(
+                    (retrieveRes) => {
+                        traitment.status = StatusEnum.FINISHED;
+                        // Process the payload as needed
+                        const bin: Buffer = Buffer.from(
+                            retrieveRes.data,
+                            'binary',
+                        );
+                        fs.writeFileSync(
+                            `public/downloads/${traitment.timestamp}.tar.gz`,
+                            bin,
+                        );
+                        console.log('mail sent to smtp');
+                        const mailOptions: EmailOptions = {
+                            to: traitment.mail,
+                            subject: config.mailSuccess.subject,
+                            text:
+                                config.mailSuccess.text +
+                                `\n ${req.protocol}://${req.hostname}/downloads/${traitment.timestamp}.tar.gz`,
+                        };
+                        sendEmail(mailOptions);
+                        res.send('Email envoyé');
+                    },
+                    (error) => {
+                        console.log(error);
+                        res.status(500).send('Error retrieving file');
+                    },
+                );
+        } else {
+            console.log(
+                `This traitment doesn't exist on current process id=${id}`,
+            );
+            res.status(404).send(
+                `This traitment doesn't exist on current process id=${id}`,
+            );
+        }
+    },
+    (error) => {
+        console.log(error);
+    },
+);
 
 //Webhook of failure after traitment done
-router.post('/failure', (req, res) => {
-    const { id } = req.query;
-    const traitment: Traitment = currentTraitments.filter((traitment) => traitment.timestamp === id)[0];
-    traitment.status = StatusEnum.FINISHED_ERROR;
-    if (traitment) {
-        currentTraitments = currentTraitments.filter((t) => t.timestamp !== traitment.timestamp);
-        // Process the payload as needed
-        console.log('Received webhook error:', id);
-        const mailOptions: EmailOptions = { to: traitment.mail, subject: config.mailError.subject, text: config.mailError.text };
-        sendEmail(mailOptions);
-    }
-}, (error) => {
-    console.log(error);
-});
-
-
+router.post(
+    '/failure',
+    (req, res) => {
+        const { id } = req.query;
+        const traitment: Traitment = currentTraitments.filter(
+            (traitment) => traitment.timestamp === id,
+        )[0];
+        traitment.status = StatusEnum.FINISHED_ERROR;
+        if (traitment) {
+            currentTraitments = currentTraitments.filter(
+                (t) => t.timestamp !== traitment.timestamp,
+            );
+            // Process the payload as needed
+            console.log('Received webhook error:', id);
+            const mailOptions: EmailOptions = {
+                to: traitment.mail,
+                subject: config.mailError.subject,
+                text: config.mailError.text,
+            };
+            sendEmail(mailOptions);
+        }
+    },
+    (error) => {
+        console.log(error);
+    },
+);
 
 module.exports = router;
