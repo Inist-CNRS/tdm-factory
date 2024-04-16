3d4bd7874c40a2b188e2ffb837ce29ddd2d824e2
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-04-16 15:27:49 +0200

    feat: add types into tdm-types

--- tdm-types/type/index.d.ts
+++ tdm-types/type/index.d.ts
@@ -1,1 +1,56 @@
 // TODO Add every request and response from the api
+
+type EmptyRequest = never;
+
+type ApiOperationsWrapperResponse = {
+    id: string;
+    title: string;
+    summary: string;
+    defaultValue: string;
+    mines: string[];
+};
+
+type ApiOperationsEnrichmentResponse = {
+    id: string;
+    title: string;
+    summary: string;
+};
+
+type ApiProcessingFieldsRequest = {
+    id: string;
+};
+
+type ApiProcessingFieldsResponse = {
+    fields: string[];
+};
+
+export type GET = {
+    Api: {
+        Operations: {
+            Wrapper: {
+                Request: EmptyRequest;
+                Response: ApiOperationsWrapperResponse;
+            };
+            Enrichment: {
+                Request: EmptyRequest;
+                Response: ApiOperationsEnrichmentResponse;
+            };
+        };
+        Processing: {
+            Fields: {
+                Request: ApiProcessingFieldsRequest;
+                Response: ApiProcessingFieldsResponse;
+            };
+        };
+    };
+};
+
+type UploadResponse = {
+    id: string;
+};
+
+export type POST = {
+    Upload: {
+        Response: UploadResponse;
+    };
+};
