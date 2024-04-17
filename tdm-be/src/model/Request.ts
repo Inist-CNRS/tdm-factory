739b7fdb091d590dcc9ed52fa2e4db596adc7173
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-04-17 07:37:39 +0200

    fix: correctly handle wrapper param

--- tdm-be/src/model/Request.ts
+++ tdm-be/src/model/Request.ts
@@ -1,13 +1,6 @@
-export class Parameter {
-    displayName?: string;
-    name?: string;
-    value?: string;
-}
-
 export class Request {
     label?: string;
     url = '';
-    parameters?: Parameter[];
-    fileType?: string[];
+    parameters?: string[];
     description?: string;
 }
