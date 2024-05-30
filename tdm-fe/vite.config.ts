96ebcc6bf5a0b7896649a44dff3d0ac9f3939e1a
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-05-30 15:42:55 +0200

    feat: add gzip and brotli to create static files

--- tdm-fe/vite.config.ts
+++ tdm-fe/vite.config.ts
@@ -1,11 +1,17 @@
 import react from '@vitejs/plugin-react';
 import md5 from 'md5';
+import gzipPlugin from 'rollup-plugin-gzip';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import stylelint from 'vite-plugin-stylelint';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const regex = /(.*node_modules\/)([^\/]+)(.*)/;
+import { promisify } from 'node:util';
+import { brotliCompress } from 'node:zlib';
+
+const brotliPromise = promisify(brotliCompress);
+
+const regex = /(.*node_modules\/)([^/]+)(.*)/;
 
 const linter =
     process.env.VITE_ENV === 'prod'
@@ -18,12 +24,27 @@ const linter =
           ];
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths(), ...linter],
-    css: {
-        devSourcemap: process.env.VITE_SOURCE_MAP === 'true',
-    },
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        gzipPlugin({
+            customCompression: (content) => brotliPromise(Buffer.from(content)),
+            fileName: '.br',
+            minSize: 1000,
+            gzipOptions: {
+                level: 9,
+            },
+        }),
+        gzipPlugin({
+            minSize: 1000,
+            gzipOptions: {
+                level: 9,
+            },
+        }),
+        ...linter,
+    ],
     build: {
-        sourcemap: process.env.VITE_SOURCE_MAP === 'true',
+        sourcemap: true,
         rollupOptions: {
             output: {
                 manualChunks: (id, meta) => {
@@ -36,13 +57,13 @@ export default defineConfig({
         },
         minify: 'terser',
         terserOptions: {
-            sourceMap: process.env.VITE_SOURCE_MAP === 'true',
-            ecma: 2018,
+            sourceMap: true,
+            ecma: 2020,
             compress: {
-                ecma: 2018,
+                ecma: 2020,
             },
             format: {
-                ecma: 2018,
+                ecma: 2020,
             },
         },
     },
