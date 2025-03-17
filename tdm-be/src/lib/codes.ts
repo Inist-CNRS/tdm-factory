5a8ef37c133cb3e63d063fca0e9e3ae94674ae70
Mattheo Termine <mattheo.termine@inist.fr>
2025-03-17 15:22:12 +0100

    feat: changed error codes

--- tdm-be/src/lib/codes.ts
+++ tdm-be/src/lib/codes.ts
@@ -1,31 +1,24 @@
 // User input error
-export const ERROR_CODE_WRAPPER_BAD_USER_INPUT = '40';
-export const ERROR_MESSAGE_WRAPPER_BAD_USER_INPUT = `Le convertisseur a rencontré une erreur inattendue. Code d'erreur ${ERROR_CODE_WRAPPER_BAD_USER_INPUT}`;
+export const ERROR_MESSAGE_WRAPPER_BAD_USER_INPUT = 'Le convertisseur a rencontré une erreur inattendue due à une entrée utilisateur invalide.';
 
 // Server Error
-export const ERROR_CODE_WRAPPER_UNEXPECTED_ERROR = '50';
-export const ERROR_MESSAGE_WRAPPER_UNEXPECTED_ERROR = `Le convertisseur a rencontré une erreur inattendue. Code d'erreur ${ERROR_CODE_WRAPPER_UNEXPECTED_ERROR}`;
+export const ERROR_MESSAGE_WRAPPER_UNEXPECTED_ERROR = 'Le convertisseur a rencontré une erreur inattendue.';
 
-export const ERROR_CODE_FILE_SYSTEM_ERROR = '51';
-export const ERROR_MESSAGE_FILE_SYSTEM_ERROR = `Le traitement a rencontré une erreur inattendue. Code d'erreur ${ERROR_CODE_FILE_SYSTEM_ERROR}`;
+export const ERROR_MESSAGE_FILE_SYSTEM_ERROR = 'Le traitement a rencontré une erreur inattendue liée au système de fichiers.';
 
-export const ERROR_CODE_WRAPPER_UNREACHABLE_ERROR = '52';
-export const ERROR_MESSAGE_WRAPPER_UNREACHABLE_ERROR = `Le convertisseur a rencontré une erreur inattendue. Code d'erreur ${ERROR_CODE_WRAPPER_UNREACHABLE_ERROR}`;
+export const ERROR_MESSAGE_WRAPPER_UNREACHABLE_ERROR = 'Le convertisseur est actuellement inaccessible.';
 
-export const ERROR_CODE_ENRICHMENT_UNREACHABLE_ERROR = '53';
-export const ERROR_MESSAGE_ENRICHMENT_UNREACHABLE_ERROR = `L'enrichissement a rencontré une erreur inattendue. Code d'erreur ${ERROR_CODE_ENRICHMENT_UNREACHABLE_ERROR}`;
+export const ERROR_MESSAGE_ENRICHMENT_UNREACHABLE_ERROR = "L'enrichissement est actuellement inaccessible.";
 
-export const ERROR_CODE_ENRICHMENT_PAYLOAD_NOT_ACCEPTED_ERROR = '54';
-export const ERROR_MESSAGE_ENRICHMENT_PAYLOAD_NOT_ACCEPTED_ERROR = `L'enrichissement a refusé le traitement. Code d'erreur ${ERROR_CODE_ENRICHMENT_PAYLOAD_NOT_ACCEPTED_ERROR}`;
+export const ERROR_MESSAGE_ENRICHMENT_PAYLOAD_NOT_ACCEPTED_ERROR = "L'enrichissement a refusé le traitement des données.";
 
-export const ERROR_CODE_ENRICHMENT_UNEXPECTED_ERROR = '55';
-export const ERROR_MESSAGE_ENRICHMENT_UNEXPECTED_ERROR = `L'enrichissement a rencontré une erreur inattendue. Code d'erreur ${ERROR_CODE_ENRICHMENT_UNEXPECTED_ERROR}`;
+export const ERROR_MESSAGE_ENRICHMENT_UNEXPECTED_ERROR = "L'enrichissement a rencontré une erreur inattendue.";
 
-export const ERROR_CODE_ENRICHMENT_HOOK_UNEXPECTED_ERROR = '56';
-export const ERROR_MESSAGE_ENRICHMENT_HOOK_UNEXPECTED_ERROR = `La récuperation des données a rencontré une erreur inattendue. Code d'erreur ${ERROR_CODE_ENRICHMENT_HOOK_UNEXPECTED_ERROR}`;
+export const ERROR_MESSAGE_ENRICHMENT_HOOK_UNEXPECTED_ERROR = 'La récupération des données a rencontré une erreur inattendue.';
 
-export const ERROR_CODE_ENRICHMENT_HOOK_UNREACHABLE_ERROR = '57';
-export const ERROR_MESSAGE_ENRICHMENT_HOOK_UNREACHABLE_ERROR = `La récuperation des données a rencontré une erreur inattendue. Code d'erreur ${ERROR_CODE_ENRICHMENT_HOOK_UNREACHABLE_ERROR}`;
+export const ERROR_MESSAGE_ENRICHMENT_HOOK_UNREACHABLE_ERROR = 'Le service de récupération des données est actuellement inaccessible.';
 
-export const ERROR_CODE_ENRICHMENT_HOOK_PAYLOAD_NOT_ACCEPTED_ERROR = '58';
-export const ERROR_MESSAGE_ENRICHMENT_HOOK_PAYLOAD_NOT_ACCEPTED_ERROR = `La récuperation des données a été refusé. Code d'erreur ${ERROR_CODE_ENRICHMENT_HOOK_PAYLOAD_NOT_ACCEPTED_ERROR}`;
+export const ERROR_MESSAGE_ENRICHMENT_HOOK_PAYLOAD_NOT_ACCEPTED_ERROR = 'La récupération des données a été refusée.';
+
+export const ERROR_MESSAGE_ENRICHMENT_WEBHOOK_TIMEOUT = 
+    "Le service d'enrichissement n'a pas répondu dans le délai imparti";
