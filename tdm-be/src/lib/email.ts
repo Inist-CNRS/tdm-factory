ca78faca6c41a631f63689433f536e3b68119630
Mattheo Termine <mattheo.termine@inist.fr>
2025-03-17 13:40:36 +0100

    feat(email): add html email templates

--- tdm-be/src/lib/email.ts
+++ tdm-be/src/lib/email.ts
@@ -40,12 +40,14 @@ export type StartedMailOptions = {
 
 export const sendStartedMail = async (options: StartedMailOptions) => {
     try {
-        const text = nunjucks.render('processing-started.njk', options.data);
+        const html = nunjucks.render('processing-started.njk', options.data);
+        const text = nunjucks.render('processing-started-text.njk', options.data);
 
         await transporter.sendMail({
             from: environment.mailFrom ?? 'dev@local',
             to: options.email,
             subject: `IA Factory - Notification de creation - Traitement ${options.data.processingId}`,
+            html,
             text,
         });
         mailLogger.info(`Notification email for processing '${options.data.processingId}' send`);
@@ -63,12 +65,14 @@ export type FinishedMailOptions = {
 
 export const sendFinishedMail = async (options: FinishedMailOptions) => {
     try {
-        const text = nunjucks.render('processing-finished.njk', options.data);
+        const html = nunjucks.render('processing-finished.njk', options.data);
+        const text = nunjucks.render('processing-finished-text.njk', options.data);
 
         await transporter.sendMail({
             from: environment.mailFrom ?? 'dev@local',
             to: options.email,
             subject: `IA Factory - Résultat - Traitement ${options.data.processingId}`,
+            html,
             text,
         });
         mailLogger.info(`Result email for processing '${options.data.processingId}' send`);
@@ -86,12 +90,14 @@ export type ErrorMailOptions = {
 
 export const sendErrorMail = async (options: ErrorMailOptions) => {
     try {
-        const text = nunjucks.render('processing-error.njk', options.data);
+        const html = nunjucks.render('processing-error.njk', options.data);
+        const text = nunjucks.render('processing-error-text.njk', options.data);
 
         await transporter.sendMail({
             from: environment.mailFrom ?? 'dev@local',
             to: options.email,
             subject: `IA Factory - Rapport d'erreur - Traitement ${options.data.processingId}`,
+            html,
             text,
         });
         mailLogger.info(`Error email for processing '${options.data.processingId}' send`);
