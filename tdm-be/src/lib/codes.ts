cd285fdd31d9d6d50c31efccafd07534ca16a6e5
François Parmentier <francois.parmentier@gmail.com>
2025-03-18 09:04:44 +0100

    fix(codes): Error messages wrongly formatted

--- tdm-be/src/lib/codes.ts
+++ tdm-be/src/lib/codes.ts
@@ -1,24 +1,27 @@
 // User input error
-export const ERROR_MESSAGE_WRAPPER_BAD_USER_INPUT = 'Le format du fichier fourni n\'était pas le bon.';
+export const ERROR_MESSAGE_WRAPPER_BAD_USER_INPUT = "Le format du fichier fourni n'était pas le bon.";
 
 // Server Error
-export const ERROR_MESSAGE_WRAPPER_UNEXPECTED_ERROR = 'Le changement de format du fichier s'est mal passé.';
+export const ERROR_MESSAGE_WRAPPER_UNEXPECTED_ERROR = "Le changement de format du fichier s'est mal passé.";
 
-export const ERROR_MESSAGE_FILE_SYSTEM_ERROR = 'Le traitement n'a pas pu lire le fichier après changement de format.';
+export const ERROR_MESSAGE_FILE_SYSTEM_ERROR = "Le traitement n'a pas pu lire le fichier après changement de format.";
 
 export const ERROR_MESSAGE_WRAPPER_UNREACHABLE_ERROR = 'Le convertisseur est actuellement inaccessible.';
 
 export const ERROR_MESSAGE_ENRICHMENT_UNREACHABLE_ERROR = "L'enrichissement est actuellement inaccessible.";
 
-export const ERROR_MESSAGE_ENRICHMENT_PAYLOAD_NOT_ACCEPTED_ERROR = "L'enrichissement a refusé le traitement des données.";
+export const ERROR_MESSAGE_ENRICHMENT_PAYLOAD_NOT_ACCEPTED_ERROR =
+    "L'enrichissement a refusé le traitement des données.";
 
 export const ERROR_MESSAGE_ENRICHMENT_UNEXPECTED_ERROR = "L'enrichissement a rencontré une erreur inattendue.";
 
-export const ERROR_MESSAGE_ENRICHMENT_HOOK_UNEXPECTED_ERROR = 'La récupération des données a rencontré une erreur inattendue.';
+export const ERROR_MESSAGE_ENRICHMENT_HOOK_UNEXPECTED_ERROR =
+    'La récupération des données a rencontré une erreur inattendue.';
 
-export const ERROR_MESSAGE_ENRICHMENT_HOOK_UNREACHABLE_ERROR = 'Le service de récupération des données est actuellement inaccessible.';
+export const ERROR_MESSAGE_ENRICHMENT_HOOK_UNREACHABLE_ERROR =
+    'Le service de récupération des données est actuellement inaccessible.';
 
 export const ERROR_MESSAGE_ENRICHMENT_HOOK_PAYLOAD_NOT_ACCEPTED_ERROR = 'La récupération des données a été refusée.';
 
-export const ERROR_MESSAGE_ENRICHMENT_WEBHOOK_TIMEOUT = 
+export const ERROR_MESSAGE_ENRICHMENT_WEBHOOK_TIMEOUT =
     "Le service d'enrichissement n'a pas répondu dans le délai imparti";
