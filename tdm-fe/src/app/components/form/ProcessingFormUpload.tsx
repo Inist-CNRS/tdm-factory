9f3a0e0f52a698a97256e18b5b2fc0783f7719dc
Mattheo Termine <mattheo.termine@inist.fr>
2025-04-25 13:30:20 +0200

    fix(typo): stringified instead of stringifies

--- tdm-fe/src/app/components/form/ProcessingFormUpload.tsx
+++ tdm-fe/src/app/components/form/ProcessingFormUpload.tsx
@@ -43,7 +43,7 @@ const ProcessingFormUpload = ({ mimes, value, isOnError, isPending, onChange, se
         queryFn: getStaticConfig,
     });
 
-    const stringifiesMimeTypes = useMemo(() => {
+    const stringifiedMimeTypes = useMemo(() => {
         return mimes.join(', ');
     }, [mimes]);
 
@@ -137,7 +137,7 @@ const ProcessingFormUpload = ({ mimes, value, isOnError, isPending, onChange, se
                                 error={isInvalid ? hasAttemptedUpload : false}
                                 hideSizeText
                                 inputProps={{
-                                    accept: stringifiesMimeTypes,
+                                    accept: stringifiedMimeTypes,
                                 }}
                                 InputProps={{
                                     sx: { display: 'none' },
@@ -177,7 +177,7 @@ const ProcessingFormUpload = ({ mimes, value, isOnError, isPending, onChange, se
                 {hasAttemptedUpload && (isInvalid || formatError) ? (
                     <div className="error-message">
                         {isInvalid ? (
-                            <p>Le fichier ne correspond pas à un format compatible, utilisez l&apos;un de ces formats : {stringifiesMimeTypes}.</p>
+                            <p>Le fichier ne correspond pas à un format compatible, utilisez l&apos;un de ces formats : {stringifiedMimeTypes}.</p>
                         ) : null}
                         {formatError && selectedFormat && config ? (
                             <p>
