ea15e673beb14ce415414b5debe53fae7ea8bcdc
François Parmentier <francois.parmentier@gmail.com>
2025-12-04 09:32:18 +0100

    refactor(fe): Optimize number of queries

--- tdm-fe/src/app/services/config.ts
+++ tdm-fe/src/app/services/config.ts
@@ -1,24 +1,20 @@
 import type { StaticConfig } from '~/lib/config';
 
 export const getStaticConfig = async (): Promise<StaticConfig> => {
-    let response = null;
-    response = await fetch('/config-static');
+    let response = await fetch('/config-static');
     if (!response.ok) {
         throw new Error('Failed to fetch static configuration');
     }
-    // detect when we are in dev mode and use the localhost url
-    const content = await response.text();
-    if (content.startsWith('<!DOCTYPE html>')) {
+    let data;
+    try {
+        data = await response.json();
+    } catch {
+        // when we are in dev mode, use the localhost url
         response = await fetch('http://localhost:3000/config-static');
         if (!response.ok) {
             throw new Error('Failed to fetch static configuration from localhost');
         }
-    } else {
-        response = await fetch('/config-static');
-        if (!response.ok) {
-            throw new Error('Failed to fetch static configuration');
-        }
+        data = await response.json();
     }
-    const data = await response.json();
     return data;
 };
