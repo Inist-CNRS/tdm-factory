bdd4a27b7202165885e9a2d93b36975323ac9d2b
François Parmentier <francois.parmentier@gmail.com>
2025-12-04 08:23:03 +0100

    fix(fe): get StaticConfig even in dev mode

--- tdm-fe/src/app/services/config.ts
+++ tdm-fe/src/app/services/config.ts
@@ -1,28 +1,19 @@
-// import { createQuery, json } from '~/app/services/Environment';
-
 import type { StaticConfig } from '~/lib/config';
 
-// export const getStaticConfig = async (): Promise<StaticConfig> => {
-//     const response = await fetch(createQuery('/config-static'));
-//     if (!response.ok) {
-//         throw new Error('Failed to fetch static configuration');
-//     }
-//     return await json<StaticConfig>(response);
-// };
-
 export const getStaticConfig = async (): Promise<StaticConfig> => {
-    let response = await fetch('/config-static');
-    // TODO: detect when we are in dev mode and use the localhost url
+    let response = null;
+    response = await fetch('/config-static');
     if (!response.ok) {
         throw new Error('Failed to fetch static configuration');
     }
-    let data = await response.json();
-    if (!data) {
+    // TODO: detect when we are in dev mode and use the localhost url
+    const content = await response.text();
+    if (content.startsWith('<!DOCTYPE html>')) {
         response = await fetch('http://localhost:3000/config-static');
         if (!response.ok) {
             throw new Error('Failed to fetch static configuration from localhost');
         }
-        data = await response.json();
     }
+    const data = await response.json();
     return data;
 };
